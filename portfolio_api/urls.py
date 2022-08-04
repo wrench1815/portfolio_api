4b1252e564882110f5480048b77cc1bfc99314f9
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-08-05 01:00:50 +0530

    chore: update some formatting

--- portfolio_api/urls.py
+++ portfolio_api/urls.py
@@ -1,4 +1,4 @@
-"""portfolio_api URL Configuration
+'''portfolio_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -12,7 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path
 
