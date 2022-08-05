62433e0fa2e0112a59d2ef104f73f6621223b875
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-08-05 18:23:53 +0530

    feat: add model for Category

--- category/models.py
+++ category/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Model definition for Category."""
+
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    slug = models.SlugField(max_length=100, )
+
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        pass
