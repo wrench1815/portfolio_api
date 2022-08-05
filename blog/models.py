38a6d1a4f92ea9ea9e5e80c9d51dd9f1af448880
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-08-05 19:26:34 +0530

    feta: mark slug as non editable
    create slug on pre_save signal

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from category.models import Category
 
@@ -18,7 +21,10 @@ class Post(models.Model):
 
     excerpt = models.TextField()
 
-    slug = models.SlugField(max_length=150, )
+    slug = models.SlugField(
+        max_length=150,
+        editable=False,
+    )
 
     featured_image = models.URLField()
 
@@ -45,3 +51,11 @@ class Post(models.Model):
     def __str__(self):
         """Unicode representation of Post."""
         return str(f'{self.slug}')
+
+
+@receiver(post_save, sender=Post)
+def add_post_slug(sender, instance, *args, **kwargs):
+    post_slug = str(f'{instance.title} {instance.id}')
+    post_slug = slugify(post_slug)
+
+    Post.objects.filter(id=instance.id).update(slug=post_slug)
